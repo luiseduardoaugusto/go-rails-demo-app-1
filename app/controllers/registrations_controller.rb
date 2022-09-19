7acd5b4c2139a8f4beb5a366ef1921ee683ce82d
luis augusto <luis.augusto.u@gmail.com>
2022-09-19 08:26:03 -0500

    hemos agregado ahora que se logee si es que esta registrado

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,4 +2,22 @@ class RegistrationsController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: 'Logged in thanks'
+    else
+      flash[:alert]
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.required(:user).permit(:email, :password, :password_confirmation)
+  end
+
 end
